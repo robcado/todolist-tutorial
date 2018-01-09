58f8bf16a388c77996779452475418229dd46ccd
robcado <robert.cadorette@gmail.com>
2018-01-09 15:52:49 -0600

    Persisting Data Using Codable and Plist

---
+++ Todoey/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Robert Cadorette on 1/9/18.
+//  Copyright © 2018 Robert Cadorette. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Item: Codable {
+    
+    var isDone: Bool = false
+    var title: String = ""
+    
+    
+}
--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,19 +10,16 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-   var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+   var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
-        
-        
+        print(dataFilePath)
 
+        loadItems()
     }
 
     //MARK: - Tableview Datasource Methods
@@ -33,11 +30,14 @@ class ToDoListViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = itemArray[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
         
-        
+        cell.accessoryType = item.isDone ? .checkmark : .none
         
         return cell
         
@@ -47,15 +47,10 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
+        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
+        
+        saveItems()
+        
     
         tableView.deselectRow(at: indexPath, animated: true)
 
@@ -70,13 +65,14 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen when user clicks add item button
-            if let text = textField.text {
-                self.itemArray.append(text)
-            }
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.itemArray.append(newItem)
             
-            self.tableView.reloadData()
+            self.saveItems()
+            
+            
             print("Success!")
         }
         
@@ -88,6 +84,39 @@ class ToDoListViewController: UITableViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        
+        self.tableView.reloadData()
+        
+        
+        
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+            
+            
+        }
+        
+        
+        
     }
     
 }
